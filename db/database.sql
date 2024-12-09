fb3dbb6ce6f415e2ba336a5d715c4002f2e9b5e0
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-12-09 21:00:11 +0500

    Change column names to english

--- db/database.sql
+++ db/database.sql
@@ -4,9 +4,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- patient table creation
 CREATE TABLE patients (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), 
-  ism VARCHAR(255) NOT NULL, 
-  yosh INTEGER, 
-  tibbiy_tarix TEXT, 
+  name VARCHAR(255) NOT NULL, 
+  age INTEGER, 
+  medical_history TEXT, 
   contact_info JSON, 
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -21,22 +21,22 @@ CREATE TYPE status_type AS ENUM (
 --appointments table creation
 CREATE TABLE appointments (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), 
-  bemor_identifikatori UUID REFERENCES patients(id), 
+  patient_id UUID REFERENCES patients(id), 
   doctor_name VARCHAR(255) NOT NULL, 
-  tayinlash_sanasi DATE, 
+  assigned_date DATE, 
   status status_type, 
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
   updated_at TIMESTAMP DEFAULT null
 )
 
 --insert example data
-INSERT INTO patients (id, ism, yosh, tibbiy_tarix, contact_info) VALUES ('95b03502-4ce3-4dd3-90e8-11836029fad7','John Doe', 45, 'Qandli diabet, Gipertenziya','{
+INSERT INTO patients (id, name, age, medical_history, contact_info) VALUES ('95b03502-4ce3-4dd3-90e8-11836029fad7','John Doe', 45, 'Qandli diabet, Gipertenziya','{
  "phone": "123-456-7890",
  "email": "john.doe@example.com",
  "address": "Main St, 123, Anytown, AQSH"
  }');
 
- INSERT INTO appointments (id, bemor_identifikatori, doctor_name, tayinlash_sanasi, status) VALUES ('14b03502-4fd5-4dd3-90e8-11836029fbc7', '95b03502-4ce3-4dd3-90e8-11836029fad7', 'John Doe', CURRENT_DATE, 'rejalashtirilgan');
+ INSERT INTO appointments (id, patient_id, doctor_name, assigned_date, status) VALUES ('14b03502-4fd5-4dd3-90e8-11836029fbc7', '95b03502-4ce3-4dd3-90e8-11836029fad7', 'John Doe', CURRENT_DATE, 'rejalashtirilgan');
 
 
  -- Bemor yozuvini tizimdan olib tashlang.
@@ -47,23 +47,23 @@ INSERT INTO patients (id, ism, yosh, tibbiy_tarix, contact_info) VALUES ('95b035
 SELECT * FROM patients WHERE created_at::DATE BETWEEN '2024-12-08' AND '2024-12-09'
 
 -- Muayyan tibbiy holati bo'lgan barcha bemorlarni oling.
-SELECT * FROM patients WHERE tibbiy_tarix = 'Qandli diabet, Gipertenziya'
+SELECT * FROM patients WHERE medical_history = 'Qandli diabet, Gipertenziya'
 
 -- Muayyan shifokor uchun barcha uchrashuvlarni oling.
 SELECT * FROM appointments WHERE doctor_name = 'John Doe'
 
 -- Muayyan vaqt oralig'ida qabul qilinmagan barcha bemorlarni oling.
-SELECT * FROM appointments WHERE tayinlash_sanasi::DATE NOT BETWEEN '2024-12-08' AND '2024-12-09'
+SELECT * FROM appointments WHERE assigned_date::DATE NOT BETWEEN '2024-12-08' AND '2024-12-09'
 
 -- Ikki aniq sana o'rtasida rejalashtirilgan uchrashuvlar bo'lgan bemorlarni aniqlang.
-SELECT p.* FROM patients p  INNER JOIN appointments a ON p.id = a.bemor_identifikatori AND a.status = 'rejalashtirilgan' AND a.created_at::DATE BETWEEN '2024-12-08' AND '2024-12-09'
+SELECT p.* FROM patients p  INNER JOIN appointments a ON p.id = a.patient_id AND a.status = 'rejalashtirilgan' AND a.created_at::DATE BETWEEN '2024-12-08' AND '2024-12-09'
 
 -- Kasallik tarixida qayd etilgan muayyan kasallikka chalingan bemorlarni toping.
-SELECT * FROM patients WHERE tibbiy_tarix LIKE '%Gipertenziya%'
+SELECT * FROM patients WHERE medical_history LIKE '%Gipertenziya%'
 
 -- Muayyan shifokor bilan rejalashtirilgan barcha uchrashuvlarni sanab o'ting.
 SELECT * FROM appointments WHERE doctor_name = 'John Doe'
 
 -- So'nggi bir necha oy ichida (masalan, so'nggi 6 oyda) hech qanday uchrashuv o'tkazmagan bemorlarni toping.
-SELECT p.* FROM patients p  INNER JOIN appointments a ON p.id = a.bemor_identifikatori WHERE a.created_at >= CURRENT_DATE - INTERVAL '60 days'
+SELECT p.* FROM patients p  INNER JOIN appointments a ON p.id = a.patient_id WHERE a.created_at >= CURRENT_DATE - INTERVAL '60 days'
 
